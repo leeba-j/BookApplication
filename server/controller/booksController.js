08780d7c971369f3dff855d5bbe9c8bfd8a53e17
Julius Malemela <julius@thedigitalacademy.co.za>
2021-04-22 20:55:45 +0200

    Retrieve data from the server

--- server/controller/booksController.js
+++ server/controller/booksController.js
@@ -1,13 +1,23 @@
 const booksModel = require("../model/bookModel");
 
+//Function to add a new book
 exports.createBook = (req, res) => {
+  const {title, author, description, image} = req.body;
   booksModel
     .create({
-      title: "Title",
-      author: "Author",
-      description: "Description",
-      image: "image",
+      title: title,
+      author: author,
+      description: description,
+      image: image,
     })
-    .then((data) => res.send(data))
-    .catch((error) => res.send(error));
+    .then((data) => res.json(data))
+    .catch((error) => res.json("Error: ", error));
 };
+
+//Function to retrieve all books
+exports.retrieveBooks = (req, res) => {
+  console.log("Yes");
+  booksModel.find({})
+  .then((data) => res.json(data))
+  .catch((error) => res.json("Error: ", error));
+}

93fd442204790878d5b525165b7ea26c96fbd606
Leeba Julius Malemela <Julius@thedigitalacademy.co.za>
2021-04-22 13:41:40 +0200

    Set app routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BooksComponent } from './components/books/books.component';
+import { CreateBookComponent } from './components/create-book/create-book.component';
+import { DeleteBookComponent } from './components/delete-book/delete-book.component';
+import { EditBookComponent } from './components/edit-book/edit-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'books', component: BooksComponent},
+  {path: 'createBook', component: CreateBookComponent},
+  {path:'editBook', component: EditBookComponent},
+  {path: 'deleteBook', component: DeleteBookComponent},
+  {path:'**', pathMatch:'full', component:BooksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

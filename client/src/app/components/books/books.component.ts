bcd33c6ba5a79f0e974c2eda840c575d42ed748a
Julius Malemela <julius@thedigitalacademy.co.za>
2021-04-23 01:16:46 +0200

    Added a form in modal

--- client/src/app/components/books/books.component.ts
+++ client/src/app/components/books/books.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { BookCRUDService } from 'src/app/services/book-crud.service';
 
@@ -8,8 +9,16 @@ import { BookCRUDService } from 'src/app/services/book-crud.service';
   styleUrls: ['./books.component.css'],
 })
 export class BooksComponent implements OnInit {
-  constructor(private bookCRUDService: BookCRUDService, private modalService: BsModalService) {}
+  constructor(private bookCRUDService: BookCRUDService, private modalService: BsModalService, private formBuilder: FormBuilder) {
+    this.formData = this.formBuilder.group({
+      title: [''],
+      author : [''],
+      description: [''],
+      image: ['']
+    })
+  }
   books: any;
+  formData: FormGroup;
   modalRef: BsModalRef | any;
   ngOnInit(): void {
     this.bookCRUDService.retrieveBooks().subscribe((data) => {
@@ -19,6 +28,9 @@ export class BooksComponent implements OnInit {
   deleteBook(_id: string){
     this.bookCRUDService.deleteBook(_id).subscribe(data => console.log(data));
 
+  }
+  editBook(){
+    console.log(this.formData.value);
   }
 
   openModal(template: TemplateRef<any>) {
